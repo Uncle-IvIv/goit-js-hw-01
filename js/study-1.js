f1e17149e274606a5c70d53592ec1b255bd53003
John-IvIv <econova2014@gmail.com>
2025-01-22 14:52:52 +0200

    task-all

--- js/study-1.js
+++ js/study-1.js
@@ -18,15 +18,61 @@ add(15, 27, 10);
 add(10, 20, 30);
 add(5, 10, 15);
 
-//   function makeMessage(username) {
-// 	console.log(`Hello ${username}`);
-// }
-// makeMessage("Jacob");
+function makeMessages(username) {
+  console.log(`Hello ${username}`);
+}
+makeMessages("Jacob");
 
-function makeMessage(name, price) {
+function makeMessagea(name, price) {
   console.log(`You picked ${name}, price per item is ${price} credits`);
 }
-makeMessage("Radar", 6150);
-makeMessage("Scanner", 3500);
-makeMessage("Reactor", 8000);
-makeMessage("Engine", 4070);
+makeMessagea("Radar", 6150);
+makeMessagea("Scanner", 3500);
+makeMessagea("Reactor", 8000);
+makeMessagea("Engine", 4070);
+
+function makeMessaged(name, price) {
+  return `You picked ${name}, price per item is ${price} credits`;
+}
+console.log(makeMessaged("Radar", 6150));
+console.log(makeMessaged("Scanner", 3500));
+console.log(makeMessaged("Reactor", 8000));
+console.log(makeMessaged("Engine", 4070));
+
+function calculateTotalPrice(orderedQuantity, pricePerItem) {
+  return `${orderedQuantity * pricePerItem}`;
+}
+console.log(calculateTotalPrice(5, 100));
+console.log(calculateTotalPrice(8, 60));
+console.log(calculateTotalPrice(3, 400));
+console.log(calculateTotalPrice(1, 3500));
+console.log(calculateTotalPrice(12, 70));
+
+function multiply(x, y, z) {
+  return x * y * z;
+}
+
+//   const result = multiply(2, 3, 5);return quantity, pricePerDroid
+//   console.log(result);
+// const result = makeTransaction(5, 3000);
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+
+//-- DZ-1 --//
+
+// ${}function (quantity, pricePerDroid) {return quantity * pricePerItem}
+
+// function makeTransaction(quantity, pricePerDroid)
+// function totalPrice(quantity, pricePerDroid) {"${quantity * pricePerDroid}"}
+//
+// function makeTransaction(quantity, pricePerDroid, totalPrice) { "You ordered <quantity> droids worth <totalPrice> credits!"}
+
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    pricePerDroid * quantity
+  } credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,9 @@
 //-- Ordering droids --//
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    pricePerDroid * quantity
+  } credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
